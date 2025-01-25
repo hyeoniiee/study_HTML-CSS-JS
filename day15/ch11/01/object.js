5e42e566f7eaf0e3d8d792d6c865bf331100f212
hyeoniiee <starjea0126@gmail.com>
2025-01-25 23:44:07 +0900

    day15 - 객체다루기1

---
+++ day15/ch11/01/object.js
@@ -0,0 +1,12 @@
+const person = {};
+const person1 = { name: "홍길동" };
+const person2 = { name: "홍길동", age: 20, isAdult: true };
+const person3 = {
+  name: { firstName: "길동", lastNamr: "홍" },
+  age: 20,
+};
+const person4 = {
+  printInfo: function () {
+    console.log("printInfo");
+  },
+};
---
+++ day15/ch11/string.js
@@ -0,0 +1,5 @@
+const person = {
+  name: "Hong",
+};
+person.name = "kim";
+console.log(person.name);
