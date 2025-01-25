e30093c7fa93581e926671d0bb4162b629b3d25b
hyeoniiee <starjea0126@gmail.com>
2025-01-25 23:44:21 +0900

    day16 - 객체다루기2

---
+++ day16/ch11/03/array/method.js
@@ -0,0 +1,9 @@
+const arr = [10, 20, 30, 40];
+arr.push(50); // 배열 맨 뒤에 추가
+console.log(arr);
+arr.pop(); // 배열 맨 뒤에 요소 추출
+console.log(arr);
+arr.unshift(0); // 배멸 맨 앞에 추가
+console.log(arr);
+arr.shift(); // 배열 맨 앞에 요소 추출
+console.log(arr);
---
+++ day16/ch11/string.js
@@ -0,0 +1,5 @@
+const floatNumber = 10.52;
+
+console.log(Math.floor(floatNumber)); // 내림
+console.log(Math.ceil(floatNumber)); // 올림
+console.log(Math.round(floatNumber)); // 반올림
